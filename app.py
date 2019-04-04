957e2526e4172eaec2e3d470ba3609a16d60999c
pranjaldronzer <40349163+pranjaldronzer@users.noreply.github.com>
2019-04-04 16:41:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import request, Flask, jsonify
 app = Flask(__name__)
 
+Apixu_KEY = 69d1cf64bfe445a7831103122190404
+Apixu_Request = "http://api.apixu.com/v1/forecast.json?key="+Apixu_KEY"&q="
+
 def get_nested(data, *args):
 	if args and data:
 		element  = args[0]
@@ -12,7 +15,10 @@ def get_nested(data, *args):
 def handle_POST():
 	if not request.json:
 		abort(400)
-	response = {"intent" : get_nested(request.json, "queryResult", "intent","displayName") }
+	response = ''
+	Intent = get_nested(request.json, "queryResult", "intent","displayName")
+	if Intent is 'Weather':
+		location = get_nested(request.json, "queryResult", "parameters", "location")
 	return jsonify({"response" : response}),200
 
 if __name__ == '__main__':
