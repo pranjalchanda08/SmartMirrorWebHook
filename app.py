c38e3d475331e295046f723e320f2904bf2054f3
pranjaldronzer <40349163+pranjaldronzer@users.noreply.github.com>
2019-04-07 04:02:39 +0530

    4am commit

--- app.py
+++ app.py
@@ -1,55 +1,17 @@
 from flask import request, Flask, jsonify
-import requests
-from datetime import datetime
+import intent
 
 app = Flask(__name__)
-
-def getNested(data, *args):
-	if args and data:
-		element  = args[0]
-		if element:
-			value = data.get(element)
-			return value if len(args) == 1 else getNested(value, *args[1:])
-def getDays(startDate,endDate,dFormat = '%Y-%m-%d'):
-	if (type(startDate) and type(endDate) and type(dFormat)) is not str:
-		return 'Invalid arguments type. Only sypports string'
-	else:
-		startDate = startDate.split('T')[0]
-		endDate = endDate.split('T')[0]
-		a = datetime.strptime(startDate,dFormat)
-		b = datetime.strptime(endDate,dFormat)
-		diff = b-a
-		return diff.days
-def Weather():
-	Apixu_KEY = "69d1cf64bfe445a7831103122190404"
-	Apixu_Request = "http://api.apixu.com/v1/forecast.json"
-	PARAMS = dict(key=Apixu_KEY)
-	parameters = getNested(request.json, "queryResult", "parameters")
-	location = getNested(parameters, "location")
-	if not location:
-		location = 'auto:ip'
-	PARAMS['q']=location
-	date = getNested(parameters, "date")
-	if date is not '':
-		PARAMS['days'] = date.split('T')[0]
-	else:
-		date_period = getNested(parameters, "date-period")
-		if date_period is not '':
-			days=getDays(startDate=date_period["startDate"],
-				endDate = date_period["endDate"])
-		PARAMS['days'] = str(days)
-	data = requests.get(url= Apixu_Request, params=PARAMS).json()
-	speech = "Its goning to be {}, with a feel of {}^C".format(data["current"]["condition"]["text"], data["current"]["feelslike_c"])
-	response = dict(speech= speech, displayText= speech, source= 'webhook')
-	return response
 
 @app.route('/request/dailogueflow/' , methods=['POST'])
 def handle_POST():
 	if not request.json:
 		return jsonify({"request" : "Bad request"}), 400
-	Intent = getNested(request.json, "queryResult", "intent","displayName")
+	Intent = intent.getNested(request.json, "queryResult", "intent","displayName")
 	if 'Weather' or 'weather' is Intent:
-		response = Weather()
+		response = intent.Weather(unit='C', request=request.json)
 	return jsonify(response),200
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
---
+++ intent.py
@@ -0,0 +1,61 @@
+import requests
+from datetime import datetime
+
+def getNested(data, *args):
+	if args and data:
+		element  = args[0]
+		if element:
+			value = data.get(element)
+			return value if len(args) == 1 else getNested(value, *args[1:])
+
+def getDays(startDate,endDate,dFormat = '%Y-%m-%d'):
+	if (type(startDate) and type(endDate) and type(dFormat)) is not str:
+		return 'Invalid arguments type. Only sypports string'
+	else:
+		startDate = startDate.split('T')[0]
+		endDate = endDate.split('T')[0]
+		a = datetime.strptime(startDate,dFormat)
+		b = datetime.strptime(endDate,dFormat)
+		diff = b-a
+		return diff.days
+
+def Weather(request,unit='C'):
+	Apixu_KEY = "69d1cf64bfe445a7831103122190404"
+	Apixu_Request = "http://api.apixu.com/v1/forecast.json"
+	forcast = False
+	PARAMS = dict(key=Apixu_KEY)
+	parameters = getNested(request, "queryResult", "parameters")
+	location = getNested(parameters, "location")
+	if not location:
+		location = 'auto:ip'
+	PARAMS['q']=location
+	date = getNested(parameters, "date")
+	if date is not '':
+		PARAMS['dt'] = date.split('T')[0]
+		now = datetime.now().isoformat()
+		days=getDays(now,PARAMS['dt'])
+		if days is not 0:
+			forcast = True
+	else:
+		date_period = getNested(parameters, "date-period")
+		if date_period is not '':
+			days=getDays(startDate=date_period["startDate"],
+				endDate = date_period["endDate"])
+			forcast = True
+		PARAMS['days'] = str(days)
+	data = requests.get(url= Apixu_Request, params= PARAMS).json()
+	if not forcast:
+		speech = "Its goning to be {}, with a feel of {}°{}".format(getNested(data,"current","condition","text"),
+			getNested(data,"current",("feelslike_c" if unit=='C' else "feelslike_f")), unit)
+	else:
+		forcast_data = data["forecast"]["forecastday"]
+		speech=''
+		for x in range(len(forcast_data)):
+			date= forcast_data[x]['date']
+			temp_max= forcast_data[x]['day'][("maxtemp_c" if unit=='C' else "maxtemp_f")]
+			temp_min= forcast_data[x]['day'][("mintemp_c" if unit=='C' else "mintemp_f")]
+			condition= forcast_data[x]['day']['condition']['text']
+			speech+= 'On {} it is going to be {}, with a maximum of {}°{} and a min of {}°{}. '.format(date,condition,
+				temp_max,unit,temp_min,unit)
+	response = dict(speech= speech, displayText= speech, source= 'webhook')
+	return response
