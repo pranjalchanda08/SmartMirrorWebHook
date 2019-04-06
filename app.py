b041d4df0cfd3db2974cd39aa687d961f75f2921
pranjaldronzer <40349163+pranjaldronzer@users.noreply.github.com>
2019-04-06 23:43:53 +0530

    new deplow

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ def getDays(startDate,endDate,dFormat = '%Y-%m-%d'):
 	else:
 		startDate = startDate.split('T')[0]
 		endDate = endDate.split('T')[0]
-		print(startDate)
-		print(endDate)
 		a = datetime.strptime(startDate,dFormat)
 		b = datetime.strptime(endDate,dFormat)
 		diff = b-a
@@ -25,35 +23,33 @@ def getDays(startDate,endDate,dFormat = '%Y-%m-%d'):
 def Weather():
 	Apixu_KEY = "69d1cf64bfe445a7831103122190404"
 	Apixu_Request = "http://api.apixu.com/v1/forecast.json"
-	Intent = getNested(request.json, "queryResult", "intent","displayName")
-	if 'Weather' in Intent:
-		PARAMS = dict(key=Apixu_KEY)
-		parameters = getNested(request.json, "queryResult", "parameters")
-		location = getNested(parameters, "location")
-		if not location:
-			location = 'auto:ip'
-		PARAMS['q']=location
-		date = getNested(parameters, "date")
-		if date is not '':
-			PARAMS['days'] = date.split('T')[0]
-		else:
-			date_period = getNested(parameters, "date-period")
-			if date_period is not '':
-				days=getDays(startDate=date_period["startDate"],
-					endDate = date_period["endDate"])
-			PARAMS['days'] = str(days)
-		print(PARAMS)
-		data = requests.get(url= Apixu_Request, params=PARAMS).json()
-		speech = "Its goning to be {}, with a feel of {}^C"
-		response = dict(speech= speech.format(data["current"]["condition"]["text"], data["current"]["feelslike_c"]))
-		print (response)
-		return response
+	PARAMS = dict(key=Apixu_KEY)
+	parameters = getNested(request.json, "queryResult", "parameters")
+	location = getNested(parameters, "location")
+	if not location:
+		location = 'auto:ip'
+	PARAMS['q']=location
+	date = getNested(parameters, "date")
+	if date is not '':
+		PARAMS['days'] = date.split('T')[0]
+	else:
+		date_period = getNested(parameters, "date-period")
+		if date_period is not '':
+			days=getDays(startDate=date_period["startDate"],
+				endDate = date_period["endDate"])
+		PARAMS['days'] = str(days)
+	data = requests.get(url= Apixu_Request, params=PARAMS).json()
+	speech = "Its goning to be {}, with a feel of {}^C".format(data["current"]["condition"]["text"], data["current"]["feelslike_c"])
+	response = dict(speech= speech, displayText= speech, source= 'webhook')
+	return response
 
 @app.route('/request/dailogueflow/' , methods=['POST'])
 def handle_POST():
 	if not request.json:
 		return jsonify({"request" : "Bad request"}), 400
-	response = Weather()
+	Intent = getNested(request.json, "queryResult", "intent","displayName")
+	if 'Weather' or 'weather' is Intent:
+		response = Weather()
 	return jsonify(response),200
 if __name__ == '__main__':
 	app.run(debug=True)
